0d1a04f2c29a486d95c7a271b3fd4e46db501264
mrchudy <chudymikolaj06@gmail.com>
2019-06-28 21:18:25 +0200

    Extract Clock component

---
+++ src/components/Clock.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+function Clock({ className = "", time }) {
+  const printZero = (val, placesCount) => {
+    var output = String(val);
+
+    while (output.length < placesCount) {
+      output = `${"0" + output}`;
+    }
+
+    return output;
+  };
+
+  const blockLimitTime = time => {
+    const maxTime = 24 * 60 * 60 * 1000 - 1;
+
+    return Math.min(Math.max(time, 0), maxTime);
+  };
+
+  const formatLimitMiliSeconds = runTime =>
+    printZero(Math.floor(runTime % 1000), 3);
+
+  const formatLimitSeconds = runTime =>
+    printZero(Math.floor(runTime / 1000) % 60, 2);
+
+  const formatLimitMinuts = runTime =>
+    printZero(Math.floor(runTime / (60 * 1000)) % 60, 2);
+
+  const formatLimitHours = runTime =>
+    printZero(Math.floor((runTime / (60 * 60 * 1000)) % 24), 2);
+
+  const runTime = blockLimitTime(time);
+  const miliSeconds = formatLimitMiliSeconds(runTime);
+  const seconds = formatLimitSeconds(runTime);
+  const minutes = formatLimitMinuts(runTime);
+  const hours = formatLimitHours(runTime);
+
+  return (
+    <h2 className={`Clock ${className}`}>
+      {`Pozostało ${hours}:${minutes}:${seconds}:${miliSeconds}`}
+    </h2>
+  );
+}
+
+export default Clock;
--- src/index.js
+++ src/index.js
@@ -2,54 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import uuid from 'uuid';
 
+import Clock from './components/Clock';
+
 function PageTitle() {
   return (
     <>
       <h1>Kurs React.js Tydzień 2</h1>
       <hr />
     </>
-  );
-}
-
-function Clock({ className = "", time }) {
-  const printZero = (val, placesCount) => {
-    var output = String(val);
-
-    while (output.length < placesCount) {
-      output = `${"0" + output}`;
-    }
-
-    return output;
-  };
-
-  const blockLimitTime = time => {
-    const maxTime = 24 * 60 * 60 * 1000 - 1;
-
-    return Math.min(Math.max(time, 0), maxTime);
-  };
-
-  const formatLimitMiliSeconds = runTime =>
-    printZero(Math.floor(runTime % 1000), 3);
-
-  const formatLimitSeconds = runTime =>
-    printZero(Math.floor(runTime / 1000) % 60, 2);
-
-  const formatLimitMinuts = runTime =>
-    printZero(Math.floor(runTime / (60 * 1000)) % 60, 2);
-
-  const formatLimitHours = runTime =>
-    printZero(Math.floor((runTime / (60 * 60 * 1000)) % 24), 2);
-
-  const runTime = blockLimitTime(time);
-  const miliSeconds = formatLimitMiliSeconds(runTime);
-  const seconds = formatLimitSeconds(runTime);
-  const minutes = formatLimitMinuts(runTime);
-  const hours = formatLimitHours(runTime);
-
-  return (
-    <h2 className={`Clock ${className}`}>
-      {`Pozostało ${hours}:${minutes}:${seconds}:${miliSeconds}`}
-    </h2>
   );
 }
 
