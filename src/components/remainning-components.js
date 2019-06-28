eb1207eb6f3a50594a07b413df3fa7010695c670
mrchudy <chudymikolaj06@gmail.com>
2019-06-28 22:26:42 +0200

    Extract TimeboxCreator to component

--- src/components/remainning-components.js
+++ src/components/remainning-components.js
@@ -1,45 +1,5 @@
 import React from 'react';
-import uuid from 'uuid';
-
-class TimeBoxCreator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.titleInput = React.createRef();
-    this.totalTimeInput = React.createRef();
-  }
-
-  handleSubmit = event => {
-    event.preventDefault();
-
-    this.props.onCreate({
-      id: uuid.v4(),
-      title: this.titleInput.current.value,
-      getTime: this.totalTimeInput.current.value
-    });
-
-    this.titleInput.current.value = "";
-    this.totalTimeInput.current.value = "";
-  };
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit} class="TimeboxCreator">
-        <label>
-          Co Robisz?
-          <input type="text" ref={this.titleInput} />
-          <br />
-        </label>
-        <label>
-          Przez jaki czas?
-          <input type="number" ref={this.totalTimeInput} />
-          <br />
-          <br />
-        </label>
-        <button>Dodajt imeBox</button>
-      </form>
-    );
-  }
-}
+import TimeBoxCreator from './TimeboxCreator';
 
 class TimeBoxList extends React.Component {
   state = {
